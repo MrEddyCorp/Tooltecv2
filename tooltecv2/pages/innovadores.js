750840c9fc07aefcf899b551b939347d6e85f203
Mr Eddy <edgarreyesrojas@gmail.com>
2023-07-21 22:14:15 -0600

    fix fetch

--- tooltecv2/pages/innovadores.js
+++ tooltecv2/pages/innovadores.js
@@ -1,8 +1,32 @@
 import Layout from '../components/layout'
 
-export default function Innovadores(){
+
+
+export const getStaticProps = async () => {
+    const res = await fetch(process.env.ULR_INNOVADORES)
+    const repo = await res.json()
+    let x = ""
+    var data = []
+    for (x in repo.expertos){
+        console.log(x)
+        data.push(repo.expertos[x])
+    }
+
+    return { props: { data } }
+  }
+
+  /*export default function Page({ repo }) {
+    return repo.expertos
+  }*/
+export default function Innovadores(props){
+   // console.log(props)
     return(
     <div>
         Innovadores
+        <ul>
+        {props.data.map((experto) => (
+          <li>{experto.nombre}</li>
+        ))}
+      </ul>
     </div>)
 }
